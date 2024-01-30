634c71c14644d685133705973fd8195549e32187
srideviGVP <44332445+srideviGVP@users.noreply.github.com>
2024-01-30 19:40:29 +0530

    app.py

--- app.py
+++ app.py
@@ -101,17 +101,17 @@ data2 = pd.read_csv("data//Salary_Data.csv")
 #data2=None
 col1, col2,col3,col4 = st.columns(4, gap="small")
 with col1:
-    st.dataframe(data1,use_container_width=True, hide_index=True)
+    st.dataframe(data1,width=500,height= 500,index=False)
 
 with col2:
    # if data2!=None:
-        st.dataframe(data2,use_container_width=True, hide_index=True)
+        st.dataframe(data2,width=500,height= 500)
 with col3:
    # if data2!=None:
-        st.dataframe(data2,use_container_width=True, hide_index=True)   
+        st.dataframe(data2,width=500,height= 500)   
 with col4:
    # if data2!=None:
-        st.dataframe(data2,use_container_width=True, hide_index=True) 
+        st.dataframe(data2,width=500,height= 500) 
 
 st.write(
     """
