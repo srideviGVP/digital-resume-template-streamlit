2dc9b1e6ea14176bf8ce9a4a6b327a13ed63c113
srideviGVP <44332445+srideviGVP@users.noreply.github.com>
2024-01-30 17:51:59 +0530

    app.py

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ profile_pic = current_dir / "assets" / "profile-pic.png"
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | John Doe"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
+NAME = "Sridevi Ravada"
 DESCRIPTION = """
 Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
 """
-EMAIL = "johndoe@email.com"
+EMAIL = "srideviravada@gvpcew.ac.in"
 SOCIAL_MEDIA = {
     "YouTube": "https://youtube.com/c/codingisfun",
     "LinkedIn": "https://linkedin.com",
