56d1a9308c13d46c7556e6a724e2565f6172298d
srideviGVP <44332445+srideviGVP@users.noreply.github.com>
2024-01-30 18:51:38 +0530

    app.py

--- app.py
+++ app.py
@@ -88,9 +88,9 @@ for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
 # --- EXPERIENCE & QUALIFICATIONS ---
 st.write('\n')
 st.subheader("Backlogs")
-data1=None
+#data1=None
 data1 = pd.read_csv("data//Salary_Data.csv")
-if data1!=None:
+if not data1.empty:
         st.dataframe(data1,width=500,height= 500)
 else:
         st.write("No Backlogs")
